8827962ea4e868a4d2330dc2239d13ad12263ca6
Ran Bi <ranbi@microsoft.com>
2021-03-30 19:29:15 +0800

    feat: add https support for android server example

--- android/app/src/main/java/org/ranbi/myapplication/MainActivity.kt
+++ android/app/src/main/java/org/ranbi/myapplication/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.ktor.util.*
 import org.ranbi.HttpProxy
+import java.security.KeyStore
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
@@ -20,7 +21,20 @@ class MainActivity : AppCompatActivity() {
     private fun runServer() {
         val proxy = HttpProxy()
 
-        // not support https for now
-        proxy.startProxy()
+        val keyStore = loadKeyStore()
+        // Start a proxy with no parameters means the server won't support https request.
+        // proxy.startProxy()
+
+        proxy.startProxy(keyStore, "mykey", "changeit")
+    }
+
+    private fun loadKeyStore(): KeyStore {
+        // this file is converted from a jks keystore
+        val openRawResource = resources.openRawResource(R.raw.temporary);
+
+        // Android doesn't support "JKS"
+        val keyStore = KeyStore.getInstance("BKS")
+        keyStore!!.load(openRawResource, "changeit".toCharArray());
+        return keyStore
     }
 }
